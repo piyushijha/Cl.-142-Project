92e6e4ff6741598fc0cdd6eebbaeeb6e6392f9a8
piyushijha <76248922+piyushijha@users.noreply.github.com>
2023-02-24 20:58:54 +0530

    Add files via upload

---
+++ demographic_filtering.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import pandas as pd 
+
+df = pd.read_csv('articles.csv')
+df = df.sort_values(['total_events'], ascending=[True])
+
+output  = df[['url', 'title', 'text', 'lang', 'total_events']].head(20).values.tolist()
+
